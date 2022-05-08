a1f3ad862a05e5b612b8f95b67f3ecdfbe7b8567
Realsammy <[realsammy86@gmail.com]>
2022-05-08 17:09:50 +0100

    Chrome url api retified

--- index.js
+++ index.js
@@ -1,39 +1,47 @@
 let myLeads = []
-let listItems = ""
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 const deleteBtn = document.getElementById("delete-btn")
-
 const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
+const tabBtn = document.getElementById("tab-btn")
 
 if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage
-    renderleads()
+    render(myLeads)
 }
 
-inputBtn.addEventListener("click", function() {
-    myLeads.push(inputEl.value)
-    inputEl.value = ""
-    render(myLeads)
+tabBtn.addEventListener("click", function(){    
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+        render(myLeads)
+    })
 })
 
-deleteBtn.addEventListener("dbclick", function() {
+function render(leads) {
+    let listItems = ""
+    for (let i = 0; i < leads.length; i++) {
+        listItems += `
+            <li>
+                <a target='_blank' href='${leads[i]}'>
+                    ${leads[i]}
+                </a>
+            </li>
+        `
+    }
+    ulEl.innerHTML = listItems
+}
+
+deleteBtn.addEventListener("dblclick", function() {
     localStorage.clear()
     myLeads = []
     render(myLeads)
 })
-// Render the leads in the unordered list using ulEl.textContent
-function render(leads) {
-    for (let i = 0; i < leads.length; i++)  {
-        listItems += `
-        <li>
-        <a target='_blank' href='${leads[i]}'>${leads[i]}
-        </a>
-        </li>
-        `
-      
 
-    }
-    ulEl.innerHTML = listItems
-}
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+    render(myLeads)
+})
